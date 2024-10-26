6a6454b0492230ec04b7adc31a6c4f23fa7df261
Milana <milana15.10.05@gmail.com>
2024-10-25 21:31:19 -0400

    Update js file
    In this commit main.js file was updated

---
+++ js/main.js
@@ -0,0 +1,66 @@
+//video
+const player = new Plyr('video');
+
+//burger-con
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+
+//contact-form 
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+const inputs = [fname, lname, email, message];
+
+
+function validateInputs() {
+    let hasError = false;
+    
+    inputs.forEach(item => {
+        if (!item.value.trim()) {
+            item.style.borderColor = "red";
+            hasError = true;
+        } else {
+            item.style.borderColor = ""; 
+        }
+    });
+
+    return hasError;
+}
+
+
+document.getElementById("submit-action").onclick = function () {
+    const hasError = validateInputs();
+
+   
+    if (!hasError) {
+        inputs.forEach(item => {
+            item.value = "";
+        });
+
+        alert("We'll get in touch soon!");
+    }
+};
+
+
+inputs.forEach(item => {
+    item.addEventListener("input", function() {
+        if (!item.value.trim()) {
+            item.style.borderColor = "red"; 
+        } else {
+            item.style.borderColor = ""; 
+        }
+    });
+});
+
